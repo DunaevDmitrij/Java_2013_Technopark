b6a7764f7f559c07dec4e2a6dbb9414a9b598d97
Артем Лобачев <artemy.l2011@gmail.com>
2013-09-20 23:30:33 +0400

    Homework done.

---
+++ src/main/java/Hello.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: artemlobachev
+ * Date: 20.09.13
+ * Time: 23:27
+ * To change this template use File | Settings | File Templates.
+ */
+public class Hello {
+    public static void main(String args[ ]){
+        System.out.println("Hello");
+    }
+}
