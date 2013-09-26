df3607872d5891f6c4801ca13e26c78ceb292cc7
Артем Лобачев <artemy.l2011@gmail.com>
2013-09-26 11:39:27 +0400

    Edded code from timer example to JettyServer.java

--- src/main/java/JettyServer.java
+++ src/main/java/JettyServer.java
@@ -13,9 +13,22 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class JettyServer  extends AbstractHandler{
+
+    public static String getTime() {
+        Date date = new Date();
+        date.getTime();
+        DateFormat formatter = new SimpleDateFormat("HH.mm.ss");
+        return formatter.format(date);
+    }
+
     public void handle(String target, Request baseRequest,
                        HttpServletRequest request,
                        HttpServletResponse response)
@@ -24,7 +37,10 @@ public class JettyServer  extends AbstractHandler{
         response.setContentType("text/html;charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
         baseRequest.setHandled(true);
-        response.getWriter().println("Hello!");
+        Map<String, Object> pageVariables = new HashMap<>();
+        pageVariables.put("refreshPeriod", "1000");
+        pageVariables.put("serverTime", getTime());
+        response.getWriter().println(PageGenerator.getPage("timer.tml", pageVariables));
     }
 
 
