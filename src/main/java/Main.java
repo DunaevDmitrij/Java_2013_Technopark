7b44ca55594a4cee96afe51b3775f6e01de03696
Артем Лобачев <artemy.l2011@gmail.com>
2013-10-07 11:07:52 +0400

    Refactoring in Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,9 +15,9 @@ import org.eclipse.jetty.servlet.ServletHolder;
 
 
 public class Main {
-    public static String STATIC_DIR = "static";
-    public static int SERVER_PORT = 8080;
-    private static int FRONTEND_THREADS_COUNT = 3;
+    public static final String STATIC_DIR = "static";
+    public static final int SERVER_PORT = 8080;
+    public static final String THREAD_NAME_FRONTEND = "Frontend";
     public static final Object threadsMonitor = new Object();
 
 
@@ -27,12 +27,9 @@ public class Main {
 
         Server server = new Server(SERVER_PORT);
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        //тут запускаем фронтенд в другом потоке(см реализацию)
-        //FrontendInThread ft = new FrontendInThread();
-        //ft.start();
         //создаем пул потоков и добавляем в него наш Frontend
         ThreadPool threadPool = new ThreadPool();
-        threadPool.add(frontend, "Frontend");
+        threadPool.add(frontend, THREAD_NAME_FRONTEND);
 
         //скармливаем его серверу
         context.addServlet(new ServletHolder(frontend), "/*");
