47ada936bde726d429db607292a1e78697af3a95
mist-sv <mist_sv@mail.ru>
2013-12-14 21:27:15 +0400

    merge

--- src/main/java/Global/mechanics/MechanicSalesImp.java
+++ src/main/java/Global/mechanics/MechanicSalesImp.java
@@ -103,6 +103,7 @@ public class MechanicSalesImp implements MechanicSales, Abonent, Runnable {
     @Override
     public void ticketBought(long requestId, boolean result) {
         this.BuyRequestsResults.put(requestId,result);
+        this.buyRequestsStatuses.put(requestId,true);
     }
 
     @Override
