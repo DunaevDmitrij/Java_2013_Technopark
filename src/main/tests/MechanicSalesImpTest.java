0de5a955d687abca423d571135a192d8bf458396
mist-sv <mist_sv@mail.ru>
2013-12-21 13:57:53 +0400

    Fix tests

--- src/main/tests/MechanicSalesImpTest.java
+++ src/main/tests/MechanicSalesImpTest.java
@@ -46,13 +46,13 @@ public class MechanicSalesImpTest {
     public void searchTest() throws InterruptedException {
         final String ErrText = "Search works wrong";
         HashMap<String, String> params = new HashMap<>();
-        params.put(MechanicSales.findParams.ARRIVAL_AIRPORT,"Аэропрт_вылета1");
-        params.put(MechanicSales.findParams.DEPARTURE_AIRPORT,"Аэропрт_вылета1");
-        params.put(MechanicSales.findParams.DEPARTURE_DATE_TIME_SINCE, "0");
+        params.put(MechanicSales.findParams.DEPARTURE_AIRPORT,"Тапоа");
+        params.put(MechanicSales.findParams.ARRIVAL_AIRPORT,"Пашковский");
+        params.put(MechanicSales.findParams.DEPARTURE_DATE_TIME_SINCE, "1356067604");
         Collection<Ticket> rez = mechanicSales.search(params);
         Thread.sleep(SLEEP_TIME);
         Assert.assertNull(ErrText, rez);
-        params.put(MechanicSales.findParams.DEPARTURE_DATE_TIME_TO,"1000");
+        params.put(MechanicSales.findParams.DEPARTURE_DATE_TIME_TO,"1419139604");
         rez = mechanicSales.search(params);
         Thread.sleep(SLEEP_TIME);
         Assert.assertEquals(ErrText, rez.size(), 1);
@@ -66,17 +66,11 @@ public class MechanicSalesImpTest {
         User tstUser = new UserImp("Valera");
 
         ArrayList<SingleTicket> singleTickets = new ArrayList<>(3);
-        //date = 7.12.13 15:00, flight time = 14hr
-        SingleTicket singleTicket = new SingleTicket("DME", "Vegas",new Date(1386428400),50400,"hangover-1", Ticket.seatClass.SEAT_CLASS_FIRST,"Boing 747-400",1);
+        SingleTicket singleTicket = new SingleTicket("Тапоа", "Пашковский",new Date(1384584530),50400, "934", Ticket.seatClass.SEAT_CLASS_ECONOMIC,"Boing 747-400",1);
         singleTickets.add(singleTicket);
-        //date = 10.12.13 12:00, flight time = 12hr
-        singleTicket = new SingleTicket("Vegas", "Bangkok",new Date(1386676800),43200,"hangover-2", Ticket.seatClass.SEAT_CLASS_ECONOMIC,"Boing 737-800",2);
+        singleTicket = new SingleTicket("Пашковский", "Bangkok",new Date(1386676800),43200,"935", Ticket.seatClass.SEAT_CLASS_ECONOMIC,"Boing 737-800",2);
         singleTickets.add(singleTicket);
-        //date = 15.12.13 10:00, flight time = 5hr
-        singleTicket = new SingleTicket("Bangkok", "MIA",new Date(1387101600),18000,"hangover-3", Ticket.seatClass.SEAT_CLASS_FIRST,"Airbus A330",3);
-        singleTickets.add(singleTicket);
-        //total time = 8d = 691200 s
-        Ticket tstTicket = new TicketImp(null, singleTickets, false, 0l);
+        Ticket tstTicket = new TicketImp(null, singleTickets, false, 0L);
 
         boolean rez = this.mechanicSales.buy(tstTicket,tstUser);
         Thread.sleep(SLEEP_TIME);
